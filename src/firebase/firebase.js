80b544d942c8895b12b757b701e2a896d7eb8a1d
Leandro Lopez <leandroe.lopez95@gmail.com>
2022-07-25 22:07:20 -0300

    add firebase to it's own file

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAXiETGrx4xYnsIUpui10I6zbcu2P0t1Sg",
+  authDomain: "react-tp-e-commerce.firebaseapp.com",
+  projectId: "react-tp-e-commerce",
+  storageBucket: "react-tp-e-commerce.appspot.com",
+  messagingSenderId: "907381526078",
+  appId: "1:907381526078:web:cbe72d49f62d52f41b0b6f"
+};
+
+// Initialize Firebase
+// eslint-disable-next-line
+const app = initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -3,22 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAXiETGrx4xYnsIUpui10I6zbcu2P0t1Sg",
-  authDomain: "react-tp-e-commerce.firebaseapp.com",
-  projectId: "react-tp-e-commerce",
-  storageBucket: "react-tp-e-commerce.appspot.com",
-  messagingSenderId: "907381526078",
-  appId: "1:907381526078:web:cbe72d49f62d52f41b0b6f"
-};
-
-// Initialize Firebase
-// eslint-disable-next-line
-const app = initializeApp(firebaseConfig);
+import './firebase/firebase'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
